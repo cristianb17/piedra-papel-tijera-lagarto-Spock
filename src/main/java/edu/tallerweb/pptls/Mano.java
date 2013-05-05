d5bb68e7ce5ad28ef07e415b1c4b8f0e3e2e0b2c
Cristian Berges <cristianb17@hotmail.com>
2013-05-05 15:32:07 -0300

    implementado

--- src/main/java/edu/tallerweb/pptls/Mano.java
+++ src/main/java/edu/tallerweb/pptls/Mano.java
@@ -8,7 +8,7 @@ import java.util.HashMap;
 public class Mano {
 
 	private Forma forma;
-	private static HashMap<String,Enum> reglasDelJuego;
+	private static HashMap<String, Enum> reglasDelJuego;
 	//me ejecuta el metodo para cargar las reglas antes que hacer cualquier otra cosa.
 	static {
 		cargarReglasDelJuego();
@@ -30,45 +30,41 @@ public class Mano {
 	 * @return un Resultado, de acuerdo al estado del juego.
 	 */
 	public Resultado jugarCon(final Mano otra) {
-		Resultado resultadoObtenidoDespuesDeJugar = (Resultado) reglasDelJuego.get(this.forma.getValor()+"VS"+otra.forma.getValor());
+		Resultado resultadoObtenidoDespuesDeJugar = (Resultado) reglasDelJuego.get(this.forma.getValor()
+																		  + "VS" + otra.forma.getValor());
 		return resultadoObtenidoDespuesDeJugar;
 	}
 	
 	//metodo utilizado para cargar las reglas del juego
 	private static void cargarReglasDelJuego(){
 		
-		reglasDelJuego = new HashMap<String,Enum>();
+		reglasDelJuego = new HashMap<String, Enum>();
 		
-		reglasDelJuego.put(Forma.LAGARTO.getValor() +"VS"+ Forma.LAGARTO.getValor(),Resultado.EMPATA);
-		reglasDelJuego.put(Forma.LAGARTO.getValor() +"VS"+ Forma.SPOCK.getValor(),Resultado.GANA);
-		reglasDelJuego.put(Forma.LAGARTO.getValor() +"VS"+ Forma.PAPEL.getValor(),Resultado.GANA);
-		reglasDelJuego.put(Forma.LAGARTO.getValor() +"VS"+ Forma.PIEDRA.getValor(),Resultado.PIERDE);
-		reglasDelJuego.put(Forma.LAGARTO.getValor() +"VS"+ Forma.TIJERA.getValor(),Resultado.PIERDE);
-		
-		reglasDelJuego.put(Forma.SPOCK.getValor() +"VS"+ Forma.SPOCK.getValor(),Resultado.EMPATA);
-		reglasDelJuego.put(Forma.SPOCK.getValor() +"VS"+ Forma.PIEDRA.getValor(),Resultado.GANA);
-		reglasDelJuego.put(Forma.SPOCK.getValor() +"VS"+ Forma.TIJERA.getValor(),Resultado.GANA);
-		reglasDelJuego.put(Forma.SPOCK.getValor() +"VS"+ Forma.LAGARTO.getValor(),Resultado.PIERDE);
-		reglasDelJuego.put(Forma.SPOCK.getValor() +"VS"+ Forma.PAPEL.getValor(),Resultado.PIERDE);
-
-		
-		reglasDelJuego.put(Forma.PAPEL.getValor() +"VS"+ Forma.PAPEL.getValor(),Resultado.EMPATA);
-		reglasDelJuego.put(Forma.PAPEL.getValor() +"VS"+ Forma.SPOCK.getValor(),Resultado.GANA);
-		reglasDelJuego.put(Forma.PAPEL.getValor() +"VS"+ Forma.PIEDRA.getValor(),Resultado.GANA);
-		reglasDelJuego.put(Forma.PAPEL.getValor() +"VS"+ Forma.LAGARTO.getValor(),Resultado.PIERDE);
-		reglasDelJuego.put(Forma.PAPEL.getValor() +"VS"+ Forma.TIJERA.getValor(),Resultado.PIERDE);
-		
-		reglasDelJuego.put(Forma.TIJERA.getValor() +"VS"+ Forma.TIJERA.getValor(),Resultado.EMPATA);
-		reglasDelJuego.put(Forma.TIJERA.getValor() +"VS"+ Forma.LAGARTO.getValor(),Resultado.GANA);
-		reglasDelJuego.put(Forma.TIJERA.getValor() +"VS"+ Forma.PAPEL.getValor(),Resultado.GANA);
-		reglasDelJuego.put(Forma.TIJERA.getValor() +"VS"+ Forma.PIEDRA.getValor(),Resultado.PIERDE);
-		reglasDelJuego.put(Forma.TIJERA.getValor() +"VS"+ Forma.SPOCK.getValor(),Resultado.PIERDE);
-		
-		reglasDelJuego.put(Forma.PIEDRA.getValor() +"VS"+ Forma.PIEDRA.getValor(),Resultado.EMPATA);
-		reglasDelJuego.put(Forma.PIEDRA.getValor() +"VS"+ Forma.LAGARTO.getValor(),Resultado.GANA);
-		reglasDelJuego.put(Forma.PIEDRA.getValor() +"VS"+ Forma.TIJERA.getValor(),Resultado.GANA);
-		reglasDelJuego.put(Forma.PIEDRA.getValor() +"VS"+ Forma.PAPEL.getValor(),Resultado.PIERDE);
-		reglasDelJuego.put(Forma.PIEDRA.getValor() +"VS"+ Forma.SPOCK.getValor(),Resultado.PIERDE);
+		reglasDelJuego.put(Forma.LAGARTO.getValor() + "VS" + Forma.LAGARTO.getValor(), Resultado.EMPATA);
+		reglasDelJuego.put(Forma.LAGARTO.getValor() + "VS" + Forma.SPOCK.getValor(), Resultado.GANA);
+		reglasDelJuego.put(Forma.LAGARTO.getValor() + "VS" + Forma.PAPEL.getValor(), Resultado.GANA);
+		reglasDelJuego.put(Forma.LAGARTO.getValor() + "VS" + Forma.PIEDRA.getValor(), Resultado.PIERDE);
+		reglasDelJuego.put(Forma.LAGARTO.getValor() + "VS" + Forma.TIJERA.getValor(), Resultado.PIERDE);
+		reglasDelJuego.put(Forma.SPOCK.getValor() + "VS" + Forma.SPOCK.getValor(), Resultado.EMPATA);
+		reglasDelJuego.put(Forma.SPOCK.getValor() + "VS" + Forma.PIEDRA.getValor(), Resultado.GANA);
+		reglasDelJuego.put(Forma.SPOCK.getValor() + "VS" + Forma.TIJERA.getValor(), Resultado.GANA);
+		reglasDelJuego.put(Forma.SPOCK.getValor() + "VS" + Forma.LAGARTO.getValor(), Resultado.PIERDE);
+		reglasDelJuego.put(Forma.SPOCK.getValor() + "VS" + Forma.PAPEL.getValor(), Resultado.PIERDE);
+		reglasDelJuego.put(Forma.PAPEL.getValor() + "VS" + Forma.PAPEL.getValor(), Resultado.EMPATA);
+		reglasDelJuego.put(Forma.PAPEL.getValor() + "VS" + Forma.SPOCK.getValor(), Resultado.GANA);
+		reglasDelJuego.put(Forma.PAPEL.getValor() + "VS" + Forma.PIEDRA.getValor(), Resultado.GANA);
+		reglasDelJuego.put(Forma.PAPEL.getValor() + "VS" + Forma.LAGARTO.getValor(), Resultado.PIERDE);
+		reglasDelJuego.put(Forma.PAPEL.getValor() + "VS" + Forma.TIJERA.getValor(), Resultado.PIERDE);
+		reglasDelJuego.put(Forma.TIJERA.getValor() + "VS" + Forma.TIJERA.getValor(), Resultado.EMPATA);
+		reglasDelJuego.put(Forma.TIJERA.getValor() + "VS" + Forma.LAGARTO.getValor(), Resultado.GANA);
+		reglasDelJuego.put(Forma.TIJERA.getValor() + "VS" + Forma.PAPEL.getValor(), Resultado.GANA);
+		reglasDelJuego.put(Forma.TIJERA.getValor() + "VS" + Forma.PIEDRA.getValor(), Resultado.PIERDE);
+		reglasDelJuego.put(Forma.TIJERA.getValor() + "VS" + Forma.SPOCK.getValor(), Resultado.PIERDE);
+		reglasDelJuego.put(Forma.PIEDRA.getValor() + "VS" + Forma.PIEDRA.getValor(), Resultado.EMPATA);
+		reglasDelJuego.put(Forma.PIEDRA.getValor() + "VS" + Forma.LAGARTO.getValor(), Resultado.GANA);
+		reglasDelJuego.put(Forma.PIEDRA.getValor() + "VS" + Forma.TIJERA.getValor(), Resultado.GANA);
+		reglasDelJuego.put(Forma.PIEDRA.getValor() + "VS" + Forma.PAPEL.getValor(), Resultado.PIERDE);
+		reglasDelJuego.put(Forma.PIEDRA.getValor() + "VS" + Forma.SPOCK.getValor(), Resultado.PIERDE);
 	}
 
 }
